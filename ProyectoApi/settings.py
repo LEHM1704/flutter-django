c63b8fee66c3856c16241b90ce0a0b5fca9a4b22
LEHM1704 <luishuancas1704@gmail.com>
2024-12-22 18:39:22 -0500

    cambios en settings

--- ProyectoApi/settings.py
+++ ProyectoApi/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',
+                 'localhost',
+                 'http://127.0.0.1:8000/',
+                  '127.0.0.1',
+                  'https://flutter-django-ventas.onrender.com',
+                  ]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
