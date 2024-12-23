f5e263d64d5fef7b4af79bb526561e9798417119
LEHM1704 <luishuancas1704@gmail.com>
2024-12-22 19:31:07 -0500

    haciendo cambios en urls.py

--- api/urls.py
+++ api/urls.py
@@ -11,6 +11,6 @@ router.register('user', UserViewSet)
 
 urlpatterns = [
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    path('login/', LoginView.as_view()),
-    path('logout/', LogoutView.as_view()),
+    path('login/', LoginView.as_view(),name='login'),
+    path('logout/', LogoutView.as_view(),name='logout'),
 ] + router.urls
